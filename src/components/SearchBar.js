ccf581973dd2d567665b534a36610f5b50c6a8be
Opeyemi Samuel <opeyemisamuel222@gmail.com>
2021-01-11 10:42:46 -0500

    adding form submit

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -3,13 +3,19 @@ class SearchBar extends Component {
   state = { term: "" };
 
   onInputChange = (e) => {
-    return this.setState({ term: e.target.value });
+    this.setState({ term: e.target.value });
+  };
+
+  onFormSubmit = (e) => {
+    e.preventDefault();
+
+    // to do. make sure you call back from parent
   };
 
   render() {
     return (
       <div className="search-bar ui segment">
-        <form action="" className="ui form">
+        <form action="" className="ui form" onSubmit={this.onFormSubmit}>
           <div className="field">
             <label htmlFor="">Video Search</label>
             <input
