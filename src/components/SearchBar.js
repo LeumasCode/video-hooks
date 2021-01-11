9e18909944b211eddadb14b7025b6ad132b9d911
Opeyemi Samuel <opeyemisamuel222@gmail.com>
2021-01-11 10:38:44 -0500

    adding event handler

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,14 +1,27 @@
-import React, { Component } from 'react'
- class SearchBar extends Component {
-    render() {
-        return (
-            <div>
-                search
-            </div>
-        )
-    }
+import React, { Component } from "react";
+class SearchBar extends Component {
+  state = { term: "" };
+
+  onInputChange = (e) => {
+    return this.setState({ term: e.target.value });
+  };
+
+  render() {
+    return (
+      <div className="search-bar ui segment">
+        <form action="" className="ui form">
+          <div className="field">
+            <label htmlFor="">Video Search</label>
+            <input
+              type="text"
+              value={this.state.term}
+              onChange={this.onInputChange}
+            />
+          </div>
+        </form>
+      </div>
+    );
+  }
 }
 
-
-
-export default SearchBar
+export default SearchBar;
