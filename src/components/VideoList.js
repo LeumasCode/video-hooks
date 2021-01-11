61391572c9e0e7bfa49bd4b7dfb6ce951fc1b1d9
Opeyemi Samuel <opeyemisamuel222@gmail.com>
2021-01-11 11:40:57 -0500

    passing state as props

---
+++ src/components/VideoList.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const VideoList = (props) => {
+    console.log(props.videos);
+    return (
+        <div>
+            videoList
+        </div>
+    )
+}
+
+export default VideoList
