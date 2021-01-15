7990c72744ada70ccaea515eb6adada0d0de9101
Opeyemi Samuel <opeyemisamuel222@gmail.com>
2021-01-15 09:06:32 -0500

    using custom hooks

--- src/components/App.js
+++ src/components/App.js
@@ -1,30 +1,29 @@
 import React, { useEffect, useState } from "react";
+import useVideos from "../hooks/useVideos";
 import SearchBar from "./SearchBar";
 import youtube from "../apis/Youtube";
 import VideoList from "./VideoList";
 import VideoDetail from "./VideoDetail";
 
 const App = () => {
- 
+  const [videos, search] = useVideos("buildings");
   const [selectedVideo, setSelectedVideo] = useState(null);
-   // setSelectedVideo(result.data.items[0]);
 
- 
- 
+  useEffect(() => {
+    setSelectedVideo(videos[0]);
+  }, [videos]);
+  // setSelectedVideo(result.data.items[0]);
 
   return (
     <div className="ui container">
-      <SearchBar onFormSubmit={onTermSubmit} />
+      <SearchBar onFormSubmit={search} />
       <div className="ui grid">
         <div className="ui row">
           <div className="eleven wide column">
             <VideoDetail video={selectedVideo} />
           </div>
           <div className="five wide column">
-            <VideoList
-              videos={videos}
-              onVideoSelect={setSelectedVideo}
-            />
+            <VideoList videos={videos} onVideoSelect={setSelectedVideo} />
           </div>
         </div>
       </div>
