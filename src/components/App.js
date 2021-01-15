85917de2c59d2965869427a44c6e44252435855a
Opeyemi Samuel <opeyemisamuel222@gmail.com>
2021-01-15 08:19:12 -0500

    removing callbacks

--- src/components/App.js
+++ src/components/App.js
@@ -23,10 +23,6 @@ const App = () => {
     setSelectedVideo(result.data.items[0]);
   };
 
-  const onVideoSelect = (video) => {
-    setSelectedVideo(video);
-  };
-
   return (
     <div className="ui container">
       <SearchBar onFormSubmit={onTermSubmit} />
@@ -36,7 +32,10 @@ const App = () => {
             <VideoDetail video={selectedVideo} />
           </div>
           <div className="five wide column">
-            <VideoList videos={videos} onVideoSelect={onVideoSelect} />
+            <VideoList
+              videos={videos}
+              onVideoSelect={setSelectedVideo}
+            />
           </div>
         </div>
       </div>
